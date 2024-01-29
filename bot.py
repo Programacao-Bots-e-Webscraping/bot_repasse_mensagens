038b5665c79941870fb37c163aec740755bd6e36
Programacao-Bots-e-Webscraping <155180699+Programacao-Bots-e-Webscraping@users.noreply.github.com>
2024-01-29 14:32:16 -0300

    mudança id dos grupos

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ sessao = 'Repassar Mensagem'
 def main():
     print('Monitoramento iniciado ...')
     client = TelegramClient(sessao, api_id, api_hash)
-    @client.on(events.NewMessage(chats = [4118772142))
+    @client.on(events.NewMessage(chats = [4118772142]))
     async def enviar_mensagem(event):
         if event.media:
-            await client.send_file(-4183296275,file = event.media,
+            await client.send_file(-1002012170582,file = event.media,
                                    caption=event.raw_text.replace('http://www.seinscrevanocanal.com.br','http://www.deixeseulike.com.br'))
         else:
-            await client.send_message(-4183296275,event.raw_text.replace('http://www.seinscrevanocanal.com.br','http://www.deixeseulike.com.br'))
+            await client.send_message(-1002012170582,event.raw_text.replace('http://www.seinscrevanocanal.com.br','http://www.deixeseulike.com.br'))
     client.start()
     client.run_until_disconnected()
     
